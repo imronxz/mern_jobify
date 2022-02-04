5f6265870309e729b1487034049e20cd649e1a46
imronxz <imronxz@gmail.com>
2022-02-04 23:18:10 +0700

    08. Config Global state Context Provider

---
+++ courses/08. Config Global state Context Provider/context/appContext.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+// TODO: global state here
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: '',
+  alertType: '',
+};
+
+//TODO: AppContext -> React.createContext()
+const AppContext = React.createContext();
+
+// TODO: context provider here with props children
+const AppProvider = ({ children }) => {
+  const [state, setState] = useState(initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state, }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// Costum Hook context
+const useAppContext = () => {
+  return useContext(AppContext);
+}
+
+export {AppProvider, initialState, useAppContext};
