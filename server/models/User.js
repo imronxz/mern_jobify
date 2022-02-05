27de24f56bd07aa4837f56f69e137dc20e1458b2
imronxz <imronxz@gmail.com>
2022-02-05 23:36:16 +0700

    19. Login User - Setup

--- server/models/User.js
+++ server/models/User.js
@@ -38,6 +38,23 @@ const UserSchema = new mongoose.Schema({
     default: 'My Location',
   },
 });
+/* 
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+ */
 
 export default mongoose.model('User', UserSchema);
