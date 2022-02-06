da7d0427524e46930c7a96a4c77b80b1fd631a52
imronxz <imronxz@gmail.com>
2022-02-06 15:44:13 +0700

    22. smallSidebar using utils - links

---
+++ client/src/utils/links.js
@@ -0,0 +1,13 @@
+import { IoBarChartSharp } from 'react-icons/io5';
+import { MdQueryStats } from 'react-icons/md';
+import { FaWpforms } from 'react-icons/fa';
+import { ImProfile } from 'react-icons/im';
+
+
+const links = [
+  { id: 1, text: 'stats', path: '/', icon: <IoBarChartSharp />, },
+  { id: 2, text: 'Semua Jobs', path: 'semua-pekerjaan', icon: <MdQueryStats />, },
+  { id: 3, text: 'Tambah Jobs', path: 'tambah-pekerjaan', icon: <FaWpforms />, },
+  { id: 4, text: 'Profile', path: 'profile-pekerja', icon: <ImProfile />, },
+];
+export default links;
